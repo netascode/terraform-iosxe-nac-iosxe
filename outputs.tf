7ef7bb1d9ca38b3240216d3fcb3cc44d9eb757f9
Navin Suvarna <nsuvarna@cisco.com>
2025-05-26 18:11:15 -0400

    Defined new OSPF outputs in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,11 @@ output "model" {
   description = "Full model."
   value       = local.model
 }
+
+output "ospf_configurations_with_vrf" {
+  value = local.ospf_configurations_with_vrf
+}
+
+output "ospf_configurations_without_vrf" {
+  value = local.ospf_configurations_without_vrf
+}
