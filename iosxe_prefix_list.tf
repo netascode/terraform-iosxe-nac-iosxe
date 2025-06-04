a4fc0b73b3f65677c42f1af6625176153f70f505
sMorrison26 <morris10@rpi.edu>
2025-06-04 16:11:06 -0400

    Reimplement prefix list & format clock

--- iosxe_prefix_list.tf
+++ iosxe_prefix_list.tf
@@ -1,19 +1,34 @@
+locals {
+  prefix_lists = flatten([
+    for device in local.devices : [
+      for prefix_list in try(local.device_config[device.name].prefix_lists, []) : {
+        key    = format("%s/%s", device.name, prefix_list.name)
+        device = device.name
+
+        prefixes = [for e in try(prefix_list.seqs, []) : {
+          name   = try(prefix_list.name, local.defaults.iosxe.configuration.prefix_lists.name, null)
+          seq    = try(e.seq, local.defaults.iosxe.configuration.prefix_lists.seqs.seq, null)
+          action = try(e.action, local.defaults.iosxe.configuration.prefix_lists.seqs.action, null)
+          ip     = try(e.ip, local.defaults.iosxe.configuration.prefix_lists.seqs.ip, null)
+          ge     = try(e.greater_equal, local.defaults.iosxe.configuration.prefix_lists.seqs.greater_equal, null)
+          le     = try(e.less_equal, local.defaults.iosxe.configuration.prefix_lists.seqs.less_equal, null)
+        }]
+
+        prefix_list_description = [for e in try(prefix_list.seqs, []) : {
+          name        = try(prefix_list.name, local.defaults.iosxe.configuration.prefix_lists.name, null)
+          description = try(prefix_list.description, local.defaults.iosxe.configuration.prefix_lists.description, null)
+        }]
+      }
+    ]
+  ])
+}
+
+
+
 resource "iosxe_prefix_list" "prefix_list" {
-  for_each = { for device in local.devices : device.name => device if try(local.device_config[device.name].prefix_lists, null) != null || try(local.defaults.iosxe.configuration.prefix_lists, null) != null }
-  device = each.value.name
-  
-  prefixes = [for e in try(local.device_config[each.value.name].prefix_lists.seqs, []): {
-    name = try(local.device_config[each.value.name].prefix_lists.name, local.defaults.iosxe.configuration.prefix_lists.name, null)
-    seq = try(e.seq, local.defaults.iosxe.configuration.prefix_lists.seqs.seq, null)
-    action = try(e.action, local.defaults.iosxe.configuration.prefix_lists.seqs.action, null)
-    ip = try(e.ip, local.defaults.iosxe.configuration.prefix_lists.seqs.ip, null)
-    ge = try(e.greater_equal, local.defaults.iosxe.configuration.prefix_lists.seqs.greater_equal, null)
-    le = try(e.less_equal, local.defaults.iosxe.configuration.prefix_lists.seqs.less_equal, null)
-  }]
+  for_each = { for e in local.prefix_lists : e.key => e }
+  device   = each.value.device
 
-  prefix_list_description = [for e in try(local.device_config[each.value.name].prefix_lists.seqs, []): {
-    name = try(local.device_config[each.value.name].prefix_lists.name, local.defaults.iosxe.configuration.prefix_lists.name, null)
-    description = try(local.device_config[each.value.name].prefix_lists.description, local.defaults.iosxe.configuration.prefix_lists.description, null)
-  }]
-
+  prefixes                = each.value.prefixes
+  prefix_list_description = each.value.prefix_list_description
 }
