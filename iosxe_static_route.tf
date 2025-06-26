4f62ec1b047bfd020cd42251df48c35c2fa99b43
jkliss-csco <jkliss@cisco.com>
2025-06-26 12:05:39 +0200

    Update iosxe_static_route.tf

--- iosxe_static_route.tf
+++ iosxe_static_route.tf
@@ -1,25 +1,24 @@
 locals {
   static_routes = flatten([
     for device in local.devices : [
-      for static_route in try(local.device_config[device.name].routing.static_routes, []) : {
+      for static_route in try(local.device_config[device.name].routing.static_routes, local.defaults.iosxe.devices.configuration.routing.static_routes, []) : {
         device_name = device.name
-        prefix      = static_route.prefix
-        mask        = static_route.mask
-        next_hops   = static_route.next_hops
+        prefix      = try(static_route.prefix, null)
+        mask        = try(static_route.mask, null)
+        next_hops   = try(static_route.next_hops, [])
+        key         = "${device.name}->${static_route.prefix}->${static_route.mask}"
       }
     ]
   ])
 }
 
-# Resource definition for configuring static routes on devices
 resource "iosxe_static_route" "static_routes" {
-  # Loop through each static route in the flattened list
   for_each = {
-    for route in local.static_routes : "${route.device_name}->${route.prefix}->${route.mask}" => route
+    for route in local.static_routes : route.key => route
   }
 
-  device    = each.value.device_name
-  prefix    = each.value.prefix
-  mask      = each.value.mask
+  device    = try(each.value.device_name, null)
+  prefix    = try(each.value.prefix, null)
+  mask      = try(each.value.mask, null)
   next_hops = try(each.value.next_hops, null)
 }
