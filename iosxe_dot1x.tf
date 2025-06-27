ac86a1be14f9d1cc30d90a55e20182636852607e
Mikey Kraus <mikekrau@cisco.com>
2025-06-27 00:43:21 -0500

    readme update and formatting

---
+++ iosxe_dot1x.tf
@@ -0,0 +1,27 @@
+resource "iosxe_dot1x" "dot1x" {
+  for_each = { for device in local.devices : device.name => device if try(local.device_config[device.name].dot1x, null) != null || try(local.defaults.iosxe.configuration.dot1x, null) != null }
+  device   = each.value.name
+
+  auth_fail_eapol                 = try(local.device_config[each.value.name].dot1x.auth_fail_eapol, local.defaults.iosxe.configuration.dot1x.auth_fail_eapol, null)
+  critical_eapol_config_block     = try(local.device_config[each.value.name].dot1x.critical_eapol_config_block, local.defaults.iosxe.configuration.dot1x.critical_eapol_config_block, null)
+  critical_recovery_delay         = try(local.device_config[each.value.name].dot1x.critical_recovery_delay, local.defaults.iosxe.configuration.dot1x.critical_recovery_delay, null)
+  delete_mode                     = try(local.device_config[each.value.name].dot1x.delete_mode, local.defaults.iosxe.configuration.dot1x.delete_mode, null)
+  test_timeout                    = try(local.device_config[each.value.name].dot1x.test_timeout, local.defaults.iosxe.configuration.dot1x.test_timeout, null)
+  logging_verbose                 = try(local.device_config[each.value.name].dot1x.logging_verbose, local.defaults.iosxe.configuration.dot1x.logging_verbose, null)
+  supplicant_controlled_transient = try(local.device_config[each.value.name].dot1x.supplicant_controlled_transient, local.defaults.iosxe.configuration.dot1x.supplicant_controlled_transient, null)
+  supplicant_force_multicast      = try(local.device_config[each.value.name].dot1x.supplicant_force_multicast, local.defaults.iosxe.configuration.dot1x.supplicant_force_multicast, null)
+  system_auth_control             = try(local.device_config[each.value.name].dot1x.system_auth_control, local.defaults.iosxe.configuration.dot1x.system_auth_control, null)
+
+  credentials = [
+    for cred in try(local.device_config[each.value.name].dot1x.credentials, []) : {
+      profile_name   = cred.profile_name
+      description    = try(cred.description, null)
+      username       = try(cred.username, null)
+      password_type  = try(cred.password_type, null)
+      password       = try(cred.password, null)
+      pki_trustpoint = try(cred.pki_trustpoint, null)
+      anonymous_id   = try(cred.anonymous_id, null)
+    }
+  ]
+
+}
