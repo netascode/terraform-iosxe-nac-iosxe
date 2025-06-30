52a5cb0ebdb974d75280609b67724fd6d3f390a9
jkliss-csco <jkliss@cisco.com>
2025-06-30 15:22:04 +0200

    Update iosxe_static_route.tf

--- iosxe_static_route.tf
+++ iosxe_static_route.tf
@@ -1,12 +1,16 @@
 locals {
   static_routes = flatten([
     for device in local.devices : [
-      for static_route in try(local.device_config[device.name].routing.static_routes, local.defaults.iosxe.devices.configuration.routing.static_routes, []) : {
+      for static_route in try(local.device_config[device.name].routing.static_routes, []) : {
         device_name = device.name
-        prefix      = try(static_route.prefix, null)
-        mask        = try(static_route.mask, null)
-        next_hops   = try(static_route.next_hops, [])
-        key         = "${device.name}->${static_route.prefix}->${static_route.mask}"
+        prefix      = try(static_route.prefix, local.defaults.iosxe.configuration.routing.static_routes.prefix, null)
+        mask        = try(static_route.mask, local.defaults.iosxe.configuration.routing.static_routes.mask, null)
+        next_hops = [for e in try(static_route.next_hops, local.defaults.iosxe.configuration.routing.static_routes.next_hops, []) : {
+          next_hop = try(e.next_hop, local.defaults.iosxe.configuration.routing.static_routes.next_hops.next_hop, null)
+        }]
+        key = format("%s/%s/%s", device.name,
+          try(static_route.prefix, local.defaults.iosxe.configuration.routing.static_routes.prefix, null),
+        try(static_route.mask, local.defaults.iosxe.configuration.routing.static_routes.mask, null))
       }
     ]
   ])
@@ -17,8 +21,8 @@ resource "iosxe_static_route" "static_routes" {
     for route in local.static_routes : route.key => route
   }
 
-  device    = try(each.value.device_name, null)
-  prefix    = try(each.value.prefix, null)
-  mask      = try(each.value.mask, null)
-  next_hops = try(each.value.next_hops, null)
+  device    = each.value.device_name
+  prefix    = each.value.prefix
+  mask      = each.value.mask
+  next_hops = each.value.next_hops
 }
