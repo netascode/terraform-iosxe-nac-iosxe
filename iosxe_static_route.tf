ac113eeef0b916da31d0b5d11587ba579a187b16
jkliss-csco <jkliss@cisco.com>
2025-07-01 13:04:48 +0200

    Update iosxe_static_route.tf

--- iosxe_static_route.tf
+++ iosxe_static_route.tf
@@ -6,7 +6,12 @@ locals {
         prefix      = try(static_route.prefix, local.defaults.iosxe.configuration.routing.static_routes.prefix, null)
         mask        = try(static_route.mask, local.defaults.iosxe.configuration.routing.static_routes.mask, null)
         next_hops = [for hop in try(static_route.next_hops, local.defaults.iosxe.configuration.routing.static_routes.next_hops, []) : {
-          next_hop = try(hop.next_hop, local.defaults.iosxe.configuration.routing.static_routes.next_hops.next_hop, null)
+          next_hop  = try(hop.next_hop, local.defaults.iosxe.configuration.routing.static_routes.next_hops.next_hop, null)
+          metric    = try(hop.metric, local.defaults.iosxe.configuration.routing.static_routes.next_hops.metric, null)
+          global    = try(hop.global, local.defaults.iosxe.configuration.routing.static_routes.next_hops.global, null)
+          name      = try(hop.name, local.defaults.iosxe.configuration.routing.static_routes.next_hops.name, null)
+          permanent = try(hop.permanent, local.defaults.iosxe.configuration.routing.static_routes.next_hops.permanent, null)
+          tag       = try(hop.tag, local.defaults.iosxe.configuration.routing.static_routes.next_hops.tag, null)
         }]
         key = format("%s/%s/%s", device.name,
           try(static_route.prefix, local.defaults.iosxe.configuration.routing.static_routes.prefix, null),
