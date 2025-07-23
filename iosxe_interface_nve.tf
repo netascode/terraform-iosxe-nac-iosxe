94c12e4e15992975a1cc04cfea373543a4ff4582
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-07-23 11:18:00 -0400

    Update default local path

--- iosxe_interface_nve.tf
+++ iosxe_interface_nve.tf
@@ -6,22 +6,22 @@ locals {
         device = device.name
 
         id                             = nve.id
-        description                    = try(nve.description, local.defaults.iosxe.configuration.nve.name.description, null)
-        shutdown                       = try(nve.shutdown, local.defaults.iosxe.configuration.nve.name.shutdown, null)
-        host_reachability_protocol_bgp = try(nve.host_reachability_protocol_bgp, local.defaults.iosxe.configuration.nve.name.host_reachability_protocol_bgp, null)
-        source_interface_loopback      = try(nve.source_interface_loopback, local.defaults.iosxe.configuration.nve.name.source_interface_loopback, null)
+        description                    = try(nve.description, local.defaults.iosxe.configuration.interfaces.nves.description, null)
+        shutdown                       = try(nve.shutdown, local.defaults.iosxe.configuration.interfaces.nves.name.shutdown, null)
+        host_reachability_protocol_bgp = try(nve.host_reachability_protocol_bgp, local.defaults.iosxe.configuration.interfaces.nves.name.host_reachability_protocol_bgp, null)
+        source_interface_loopback      = try(nve.source_interface_loopback, local.defaults.iosxe.configuration.interfaces.nves.name.source_interface_loopback, null)
 
         # Lists
         vni_vrfs = [for vni_vrf in try(nve.vni_vrfs, []) : {
           vni_range = "${vni_vrf.vni_from}-${vni_vrf.vni_to}"
-          vrf       = try(vni_vrf.vrf, local.defaults.iosxe.configuration.nve.vni_vrfs.vrf, null)
+          vrf       = try(vni_vrf.vrf, local.defaults.iosxe.configuration.interfaces.nves.vni_vrfs.vrf, null)
           }
         ]
 
         vnis = [for vni in try(nve.vnis, []) : {
           vni_range            = "${vni.vni_from}-${vni.vni_to}"
-          ipv4_multicast_group = try(vni.ipv4_multicast_group, local.defaults.iosxe.configuration.nve.vnis.ipv4_multicast_group, null)
-          ingress_replication  = try(vni.ingress_replication, local.defaults.iosxe.configuration.nve.vnis.ingress_replication, null)
+          ipv4_multicast_group = try(vni.ipv4_multicast_group, local.defaults.iosxe.configuration.interfaces.nves.vnis.ipv4_multicast_group, null)
+          ingress_replication  = try(vni.ingress_replication, local.defaults.iosxe.configuration.interfaces.nves.vnis.ingress_replication, null)
           }
         ]
     }]
