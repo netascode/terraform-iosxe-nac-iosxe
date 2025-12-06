netascode/terraform-iosxe-nac-iosxe

commit a980d9ced8dba809f6eef7680e53b482ed42fae4

feat: Add Multicast Advertise Support in Terraform Module (#97)
Co-authored-by: Jeremy Spencer <jeremspe@cisco.com>
Co-authored-by: Andrea Testino <atestini@cisco.com>

iosxe_bgp.tf
@@ -129,7 +129,7 @@ resource "iosxe_bgp_neighbor" "bgp_neighbor" {
   timers_holdtime                           = each.value.timers_holdtime
   timers_minimum_neighbor_hold              = each.value.timers_minimum_neighbor_hold
   ttl_security_hops                         = each.value.ttl_security_hops
-  update_source_interface_loopback          = each.value.update_source_interface_loopback
+  update_source_interface_loopback          = each.value.update_source_loopback
   ebgp_multihop                             = each.value.ebgp_multihop
   ebgp_multihop_max_hop                     = each.value.ebgp_multihop_max_hop
   inherit_peer_session                      = each.value.inherit_peer_session

iosxe_evpn.tf
@@ -14,6 +14,7 @@ resource "iosxe_evpn" "evpn" {
   logging_peer_state        = try(local.device_config[each.value.name].evpn.logging_peer_state, local.defaults.iosxe.configuration.evpn.logging_peer_state, null)
   route_target_auto_vni     = try(local.device_config[each.value.name].evpn.route_target_auto_vni, local.defaults.iosxe.configuration.evpn.route_target_auto_vni, null)
   anycast_gateway_mac_auto  = try(local.device_config[each.value.name].evpn.anycast_gateway_mac_auto, local.defaults.iosxe.configuration.evpn.anycast_gateway_mac_auto, null)
+  multicast_advertise       = try(local.device_config[each.value.name].evpn.multicast_advertise, local.defaults.iosxe.configuration.evpn.multicast_advertise, null)
 
   depends_on = [
     iosxe_interface_loopback.loopback
@@ -40,6 +41,7 @@ locals {
         vlan_based_ip_local_learning_enable  = try(instance.vlan_based.ip_local_learning_enable, local.defaults.iosxe.configuration.evpn.instances.vlan_based.ip_local_learning_enable, null)
         vlan_based_default_gateway_advertise = try(instance.vlan_based.default_gateway_advertise == true ? "enable" : "disable", local.defaults.iosxe.configuration.evpn.instances.vlan_based.default_gateway_advertise == true ? "enable" : "disable", null)
         vlan_based_re_originate_route_type5  = try(instance.vlan_based.re_originate_route_type5, local.defaults.iosxe.configuration.evpn.instances.vlan_based.re_originate_route_type5, null)
+        vlan_based_multicast_advertise       = try(instance.vlan_based.multicast_advertise, local.defaults.iosxe.configuration.evpn.instances.vlan_based.multicast_advertise, null)
         vlan_based_route_target_imports = try(length(instance.vlan_based.route_target_imports) == 0, true) ? null : [for rt in instance.vlan_based.route_target_imports : {
           route_target = try(rt, local.defaults.iosxe.configuration.evpn.instances.vlan_based.route_target_imports, null)
         }]
@@ -68,6 +70,7 @@ resource "iosxe_evpn_instance" "evpn_instance" {
   vlan_based_re_originate_route_type5  = each.value.vlan_based_re_originate_route_type5
   vlan_based_route_target_imports      = each.value.vlan_based_route_target_imports
   vlan_based_route_target_exports      = each.value.vlan_based_route_target_exports
+  vlan_based_multicast_advertise       = each.value.vlan_based_multicast_advertise
 }
 
 

commit a8761049602447135ad70bc03957b02be9b20b2c

fix(bgp): correct update_source variable name in bgp_neighbor resource
Fix variable name mismatch in iosxe_bgp_neighbor resource where
update_source_interface_loopback was incorrectly referencing
each.value.update_source_loopback instead of
each.value.update_source_interface_loopback.

This bug was introduced in PR #97 and causes terraform plan/apply
failures when BGP neighbor update-source configuration is used.

iosxe_bgp.tf
@@ -129,7 +129,7 @@ resource "iosxe_bgp_neighbor" "bgp_neighbor" {
   timers_holdtime                           = each.value.timers_holdtime
   timers_minimum_neighbor_hold              = each.value.timers_minimum_neighbor_hold
   ttl_security_hops                         = each.value.ttl_security_hops
-  update_source_interface_loopback          = each.value.update_source_loopback
+  update_source_interface_loopback          = each.value.update_source_interface_loopback
   ebgp_multihop                             = each.value.ebgp_multihop
   ebgp_multihop_max_hop                     = each.value.ebgp_multihop_max_hop
   inherit_peer_session                      = each.value.inherit_peer_session